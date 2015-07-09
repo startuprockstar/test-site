d0cd610ef7ccc20c7ed2178591b7253ffd954d0e
startuprockstar <bwinkler1987@gmail.com>
2015-07-08 22:01:00 -0500

    test site
    Javascript training

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/jena.jpg') {
+      myImage.setAttribute ('src','images/jasmine.jpg');
+    } else {
+      myImage.setAttribute ('src','images/jena.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName(){
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML= 'Hot girls are cool, ' + myName;
+}
+if(!localStorage.getItem('name')){
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML= 'Hot girls are cool, ' + storedName;
+}
+
+myButton.onclick= function(){
+  setUserName();
+}
